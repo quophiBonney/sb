1b1e55dc3234e0b9812dae6c58bb7aca26efe7b0
Solomon Bonney <quophiclef@gmail.com>
2023-10-19 23:43:20 +0000

    modified the faqs

--- src/pages/home/Faq.jsx
+++ src/pages/home/Faq.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Collapse } from "antd";
 import { Container } from "react-bootstrap";
-const remoteJob = `Yes and I am not just available, I am having experience with remote jobs.`;
-const location = "I am located at Kasoa-Bawjiase in the Central of Ghana.";
-const work = `I first I engage them in detailed and descriptive conversation in order to know their requirement and I also interact with during every phase in the development process.`;
+const remoteJob = `Yes I am available with full experience and taste of remote job.`;
+const location =
+  "I am in Ghana but really travel around the world for work and relaxations.";
+const work = `I first I engage them in detailed and descriptive conversation in order to know their requirements, then again interact with them during every phase in the development process.`;
+const mentoring = `My mentoring sessions are divided into three sections thus knowing thy self, theory and practicals. So before I dive into teaching you, I will first get to know you(e.g strength and weakeness) and then project the various fields present in the tech space. Once you choose your field, then the magic happens`;
 const items = [
   {
     key: "1",
@@ -24,6 +26,15 @@ const items = [
     ),
     children: <p>{work}</p>,
   },
+  {
+    key: "4",
+    label: (
+      <p className="fw-bold text-light">
+        How do you begin mentoring a beginner, student or aspirant?
+      </p>
+    ),
+    children: <p>{mentoring}</p>,
+  },
 ];
 const Faq = () => {
   const onChange = (key) => {
@@ -33,7 +44,7 @@ const Faq = () => {
     <div>
       <div className="container-fluid aboutme" id="faqs">
         <Container style={{ marginTop: "150px" }}>
-          <div className="row justify-content-center">
+          <div className="row">
             <div className="col-sm-12 col-md-12 col-lg-12 mt-5 mb-3">
               <Collapse
                 items={items}
